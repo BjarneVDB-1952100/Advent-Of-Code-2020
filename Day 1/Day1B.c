1fad792b3c08638451b5642e9c0371e2539618bf
Flopsie <bjarne.vandebroek@student.uhasselt.be>
2020-12-13 11:53:32 +0100

    Added day 1

---
+++ Day 1/Day1B.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+FILE* myFile;
+
+
+
+int AmountOfLines() {
+    int count_lines = 0;
+    char chr;
+    myFile = fopen("test.txt", "r");
+    //extract character from file and store in chr
+    chr = getc(myFile);
+    while (chr != EOF)
+    {
+        //Count whenever new line is encountered
+        if (chr == '\n')
+        {
+            count_lines = count_lines + 1;
+        }
+        //take next character from file.
+        chr = getc(myFile);
+    }
+    fclose(myFile); //close file.
+
+    return count_lines;
+}
+
+int main()
+{
+    int LengthFile = AmountOfLines();
+
+    myFile = fopen("test.txt", "r");
+
+    //read file into array
+    int numberArray[1000];
+    int i;
+
+    for (i = 0; i < LengthFile; i++)
+    {
+        fscanf(myFile, "%d", &numberArray[i]);
+    }
+
+    for (i = 0; i < LengthFile; i++)
+    {
+        for (int j = 0; j < LengthFile; j++) 
+        {
+            for (int k = 0; k < LengthFile; k++)
+            {
+                if (numberArray[i] + numberArray[j] + numberArray[k] == 2020) 
+                {
+                    printf("%d\n", numberArray[i] * numberArray[j] * numberArray[k]);
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
